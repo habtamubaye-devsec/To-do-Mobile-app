29795c31a51a894fdc5354d77fa06147cd016cc9
habtamubaye-devsec <habtamubaye2127@gmail.com>
2026-02-04 01:09:01 +0300

    feat: update SettingsScreen to include dark mode toggle functionality

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { useTheme } from "@/hooks/useTheme";
 
 const SettingsScreen = () => {
+  const { toggleDarkMode } = useTheme();
+
   return (
     <View>
       <Text>SettingsScreen</Text>
-    </View>
-  )
-}
 
-export default SettingsScreen
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text style={style.link}>Tap here to toggle Dark Mode</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SettingsScreen;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    padding: 20,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  instructionBox: {
+    marginTop: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+    padding: 20,
+    backgroundColor: "#ffffff",
+  },
+  link: {
+    marginTop: 30,
+    color: "#1e90ff",
+    fontSize: 16,
+  },
+});
