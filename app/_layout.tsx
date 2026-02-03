7cd7cfd834e871392474753923205c62fa034e69
habtamubaye-devsec <habtamubaye2127@gmail.com>
2026-02-04 01:04:57 +0300

    feat: integrate Convex client and update layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,19 @@
+import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }}>
-    <Stack.Screen
-      name="(tabs)"
-      options={{  title: "Home" }}
-    />
-    {/* <Stack.Screen
-      name="about"
-      options={{ title: "About Us" }}
-    /> */}
-  </Stack>
+  return (
+    <ConvexProvider client={convex}>
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
+      </Stack>
+    </ThemeProvider>
+    </ConvexProvider>
+  );
 }
